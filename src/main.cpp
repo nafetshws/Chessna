400fd4dc96c6d4e2a9e938b07853808ce58e0605
nafetshws <stefan0507hws21@gmail.com>
2023-05-31 13:02:51 +0200

    added test for generating attacked squares

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,12 @@
 #include "../include/types.hpp"
 
 int main(){
-    std::string fen = "rnbqkbnr/pppppppp/8/8/2B1P3/N2PBN2/PPP1QPPP/R3K2R w KQkq - 0 1";
-    Board board(fen);
+    //std::string fen = "rnbqkbnr/pppppppp/8/8/2B1P3/N2PBN2/PPP1QPPP/R3K2R w KQkq - 0 1";
+    Board board;
     
-    MoveGeneration moveGeneration(board);
-    Bitboard moves = moveGeneration.generateKingMoves(CURRENT_POSITION, WHITE);
+    MoveGeneration moveGeneration{board};
+    //Bitboard moves = moveGeneration.generateKingMoves(CURRENT_POSITION, WHITE);
+    Bitboard aSquares = moveGeneration.generateAttackedSquares(WHITE);
 
-    std::cout << "Moves: " << moves << std::endl;
+    std::cout << "Moves: " << aSquares << std::endl;
 }
