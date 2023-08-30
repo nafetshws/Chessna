ac7b964f7241e9c28b1ac0d3f6c89f58edc5362e
nafetshws <stefan0507hws21@gmail.com>
2023-08-30 23:48:10 +0200

    slightly adjusted material values

--- include/evaluation.hpp
+++ include/evaluation.hpp
@@ -3,11 +3,11 @@
 
 #include "./board.hpp"
 
-const int KING_VALUE = 100000;
+const int KING_VALUE = 20000;
 const int QUEEN_VALUE = 900;
 const int ROOK_VALUE = 500;
-const int BISHOP_VAUE = 300; 
-const int KNIGHT_VALUE = 300;
+const int BISHOP_VAUE = 330; 
+const int KNIGHT_VALUE = 320;
 const int PAWN_VALUE = 100;
 
 class Evaluation {
