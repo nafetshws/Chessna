22eb9e6d2abd35cfaafc4cda676d83a1d0e24f94
nafetshws <stefan0507hws21@gmail.com>
2023-08-31 16:08:44 +0200

    improved evaluation by rewarding adventagous piece positions

--- src/evaluation.cpp
+++ src/evaluation.cpp
@@ -18,7 +18,28 @@ int Evaluation::evaluatePosition(Board board) {
         KNIGHT_VALUE*(this->popcount(board.getKnights(c)) - this->popcount(board.getKnights(oC))) + 
         PAWN_VALUE*(this->popcount(board.getPawns(c)) - this->popcount(board.getPawns(oC)));
 
-    return materialScore * this->sideToMove;
+    int positionScore = 0; 
+        calculatePositionScore(board.getKing(c), kingMiddlGamePositionEvaluation) - calculatePositionScore(board.getKing(oC), kingMiddlGamePositionEvaluation) +
+        calculatePositionScore(board.getQueens(c), queenPositionEvaluation) - calculatePositionScore(board.getQueens(oC), queenPositionEvaluation) + 
+        calculatePositionScore(board.getRooks(c), rookPositionEvaluation) - calculatePositionScore(board.getRooks(oC), rookPositionEvaluation) + 
+        calculatePositionScore(board.getBishops(c), bishopPositionEvaluation) - calculatePositionScore(board.getBishops(oC), bishopPositionEvaluation) + 
+        calculatePositionScore(board.getKnights(c), knightPositionEvaluation) - calculatePositionScore(board.getKnights(oC), knightPositionEvaluation) + 
+        calculatePositionScore(board.getPawns(c), pawnPositionEvaluation) - calculatePositionScore(board.getPawns(oC), pawnPositionEvaluation); 
+
+    return (materialScore + positionScore) * this->sideToMove;
+}
+
+int Evaluation::calculatePositionScore(Bitboard pieces, std::vector<int> values) {
+    int sum = 0;
+    while(pieces != 0) {
+        int index = __builtin_ctzll(pieces);
+
+        sum += values.at(index);
+
+        pieces &= ~squareToBitboard(index);
+    }
+
+    return sum;
 }
 
 int Evaluation::popcount(Bitboard bitboard) {
