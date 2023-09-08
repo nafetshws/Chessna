ba20384846a32af13f0dd0676200e63063e9233e
nafetshws <stefan0507hws21@gmail.com>
2023-09-08 12:55:33 +0200

    inital setup to order moves

---
+++ include/moveOrder.hpp
@@ -0,0 +1,13 @@
+#ifndef MOVE_ORDER_H
+#define MOVE_ORDER_H
+
+#include <vector>
+#include "../include/types.hpp"
+
+
+class MoveOrder {
+    public:
+        void orderMoves(std::vector<Move>& moves); 
+};
+
+#endif
---
+++ src/moveOrder.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <vector>
+#include "../include/moveOrder.hpp"
+
+void MoveOrder::orderMoves(std::vector<Move>& moves) {
+
+}
