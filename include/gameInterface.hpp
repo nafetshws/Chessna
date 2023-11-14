166e1b8c89fd3cc107e9d4d50b127085db324c63
nafetshws <stefan0507hws21@gmail.com>
2023-11-14 11:54:22 +0100

    added abillity to easily switch betweeen own time management and custom thikning time

--- include/gameInterface.hpp
+++ include/gameInterface.hpp
@@ -5,12 +5,21 @@
 #include "../include/board.hpp"
 #include "../include/search.hpp"
 
+const float ENGINE_THINKING_TIME = 1;
+const bool ENGINE_TINKING_TIME_LIMIT = false; 
+
 class GameInterface {
     public:
         Board board;
         Search search;
         int lastMinDepth;
         static float maxTime;
+
+        bool engingeThinkingTimeLimit; 
+        float engineThinkingTime;
+
+        //necessary for pondering
+        float extraMoveTime;
 
         GameInterface();
         GameInterface(std::string fen);
